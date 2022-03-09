01b5a505bcb5d82422ffc5d5a81e9fbb7e751440
EC2 Default User <ec2-user@ip-172-31-36-47.ec2.internal>
2022-03-09 03:21:13 +0000

    {add}bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,31 @@
 class BooksController < ApplicationController
   def index
+    @book=Book.new
+    @books=Book.all
+  end
+
+  def create
+    book=Book.new(book_params)
+    book.save
+    #保存出来たら、redirect先でflashでサクセスメッセージを出す。
+    if book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to "/books/show"
+    else
+      render "/books/index"
+    end
   end
 
   def show
+    @book=Book.find(params[:id]
   end
 
   def edit
   end
+
+  private #ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to:'homes#top'
   resources:books
   get 'books'=>'books#index'
+  get 'books:id'=>'books#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
