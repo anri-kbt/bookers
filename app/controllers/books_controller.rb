b6e8158c0bbaa6d9afeb2af916374ce96f52f21b
EC2 Default User <ec2-user@ip-172-31-36-47.ec2.internal>
2022-03-09 09:01:31 +0000

    {add4}bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,7 @@ class BooksController < ApplicationController
     book.save
     #保存出来たら、redirect先でflashでサクセスメッセージを出す。
     if book.save
-      flash[:notice] = "Book was successfully created"
-      redirect_to book_path(book.id)
+      redirect_to book_path(book.id), notice:"Book was successfully created"
     else
       render "/books/index"
     end
@@ -21,7 +20,20 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book=Book.find(params{:id})
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    #リダイレクトと同時にフラッシュメッセージを表示
+    redirect_to books_path(book.id),alert:'Book was successfully destroyed.'
   end
 
   private #ストロングパラメータ
@@ -29,4 +41,5 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title,:body)
   end
 end
+
 
